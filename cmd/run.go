1eab39190382c611ff489ea784b13f5c317066bf
Victor Boissiere <victor.boissiere@qonto.com>
2023-09-06 15:14:31 +0200

    Export run function to be used as a library
    Signed-off-by: Victor Boissiere <victor.boissiere@qonto.com>

--- cmd/run.go
+++ cmd/run.go
@@ -18,31 +18,35 @@ func runCmd(configPath *string) *cobra.Command {
 		Use:   "run",
 		Short: "Run checks on current project",
 		Run: func(cmd *cobra.Command, args []string) {
-			config, err := config.New(*configPath)
-			if err != nil {
-				panic(err)
-			}
-
-			dir, err := os.Getwd()
-			if err != nil {
-				panic(err)
-			}
-
-			ruler := rules.NewRuler(config.Rules)
-
-			checker := checks.NewChecker(ruler, config.Checks, config.Groups)
-			projects := []aggregates.Project{
-				{
-					Path: ".",
-					Name: filepath.Base(dir),
-				},
-			}
-			err = checker.Run(context.Background(), projects)
-			if err != nil {
-				panic(err)
-			}
+			RunLocalCheck(*configPath)
 		},
 	}
 
 	return cmd
+}
+
+func RunLocalCheck(configPath string) {
+	config, err := config.New(configPath)
+	if err != nil {
+		panic(err)
+	}
+
+	dir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	ruler := rules.NewRuler(config.Rules)
+
+	checker := checks.NewChecker(ruler, config.Checks, config.Groups)
+	projects := []aggregates.Project{
+		{
+			Path: ".",
+			Name: filepath.Base(dir),
+		},
+	}
+	err = checker.Run(context.Background(), projects)
+	if err != nil {
+		panic(err)
+	}
 }
