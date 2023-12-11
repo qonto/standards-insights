d90e89c6a069039ccf651a702f0e540f9eb3bbba
Mathieu Corbin <mathieu.corbin@qonto.com>
2023-12-11 11:12:59 +0100

    New grep module for rules (#44)
    This commit adds a new grep module that can be used to search for
    patterns in files or directories, recursively or not.
    
    Users will be able to make the module fail if the pattern is found or
    is not found.
    
    This module shell-out to the grep CLI because it's more convenient
    than reimplementing grep in pure golang.
    
    Alpine and GolangCI lint versions are also upgraded.
    
    Signed-off-by: Mathieu Corbin <mathieu.corbin@qonto.com>

---
+++ pkg/ruler/rules/grep_test.go
@@ -0,0 +1,101 @@
+package rules_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/qonto/standards-insights/pkg/project"
+	"github.com/qonto/standards-insights/pkg/ruler/rules"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGrepRule(t *testing.T) {
+	project := project.Project{
+		Path: "./",
+	}
+
+	cases := []struct {
+		rule  rules.GrepRule
+		error string
+	}{
+		{
+			rule: rules.GrepRule{
+				Path:      "_testdata",
+				Recursive: true,
+				Pattern:   "abcdefg",
+				Match:     true,
+			},
+		},
+		{
+			rule: rules.GrepRule{
+				Path:      "_testdata",
+				Recursive: true,
+				Pattern:   "abc*",
+				Match:     true,
+			},
+		},
+		{
+			rule: rules.GrepRule{
+				Path:    "_testdata/file1",
+				Pattern: "abcdefg",
+				Match:   true,
+			},
+		},
+		{
+			rule: rules.GrepRule{
+				Path:      "_testdata",
+				Recursive: true,
+				Pattern:   "aIOJij89Yaa",
+				Match:     false,
+			},
+		},
+		{
+			rule: rules.GrepRule{
+				Path:    "_testdata/file1",
+				Pattern: "aIOJij89Yaa",
+				Match:   false,
+			},
+		},
+		{
+			rule: rules.GrepRule{
+				Path:    "_testdata",
+				Pattern: "abcdefg",
+				Match:   true,
+			},
+			error: "Is a directory",
+		},
+		{
+			rule: rules.GrepRule{
+				Path:      "_testdata",
+				Recursive: true,
+				Pattern:   "abcdefg",
+				Match:     false,
+			},
+			error: "match found for pattern",
+		},
+		{
+			rule: rules.GrepRule{
+				Path:    "_testdata/file1",
+				Pattern: "abcdefg",
+				Match:   false,
+			},
+			error: "match found for pattern",
+		},
+		{
+			rule: rules.GrepRule{
+				Path:    "_testdata/file1",
+				Pattern: "abc*",
+				Match:   false,
+			},
+			error: "match found for pattern",
+		},
+	}
+	for _, c := range cases {
+		err := c.rule.Do(context.Background(), project)
+		if c.error == "" {
+			assert.NoError(t, err)
+		} else {
+			assert.ErrorContains(t, err, c.error)
+		}
+	}
+}
