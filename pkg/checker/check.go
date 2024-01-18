5c385be7deae7e2dcd54dd809313da54c2d4ca3a
Marco Bersani <tachetefogo@gmail.com>
2024-01-18 14:02:06 +0100

    Add logical operators to checks to chain different rules
    Signed-off-by: Marco Bersani <tachetefogo@gmail.com>

--- pkg/checker/check.go
+++ pkg/checker/check.go
@@ -12,12 +12,21 @@ import (
 )
 
 func (c *Checker) executeCheck(ctx context.Context, check config.Check, project project.Project) aggregates.CheckResult {
+	if check.IsAND() {
+		return c.executeANDCheck(ctx, check, project)
+	} else {
+		return c.executeORCheck(ctx, check, project)
+	}
+}
+
+func (c *Checker) executeANDCheck(ctx context.Context, check config.Check, project project.Project) aggregates.CheckResult {
 	success := true
 	checkResult := aggregates.CheckResult{
 		Name:    check.Name,
 		Labels:  check.Labels,
 		Results: []ruleraggregates.RuleResult{},
 	}
+
 	for _, rule := range check.Rules {
 		ruleResult := c.ruler.Execute(ctx, rule, project)
 		if !ruleResult.Success {
@@ -25,6 +34,28 @@ func (c *Checker) executeCheck(ctx context.Context, check config.Check, project 
 			success = false
 		} else {
 			c.logger.Debug(fmt.Sprintf("rule %s successful on project %s for check %s", rule, project.Name, check.Name))
+		}
+		checkResult.Results = append(checkResult.Results, ruleResult)
+	}
+	checkResult.Success = success
+	return checkResult
+}
+
+func (c *Checker) executeORCheck(ctx context.Context, check config.Check, project project.Project) aggregates.CheckResult {
+	success := false
+	checkResult := aggregates.CheckResult{
+		Name:    check.Name,
+		Labels:  check.Labels,
+		Results: []ruleraggregates.RuleResult{},
+	}
+
+	for _, rule := range check.Rules {
+		ruleResult := c.ruler.Execute(ctx, rule, project)
+		if ruleResult.Success {
+			success = true
+			c.logger.Debug(fmt.Sprintf("rule %s successful on project %s for check %s", rule, project.Name, check.Name))
+		} else {
+			c.logger.Debug(fmt.Sprintf("rule %s failed on project %s for check %s", rule, project.Name, check.Name))
 		}
 		checkResult.Results = append(checkResult.Results, ruleResult)
 	}
