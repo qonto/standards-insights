67bc2191b110159aaec60c14d5586e6ed2bee934
Victor Boissiere <victor.boissiere@qonto.com>
2024-03-28 11:59:09 +0100

    Improve run command output
    Signed-off-by: Victor Boissiere <victor.boissiere@qonto.com>

--- cmd/output.go
+++ cmd/output.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/qonto/standards-insights/pkg/checker/aggregates"
 )
@@ -11,10 +12,18 @@ func stdoutResults(results []aggregates.ProjectResult) {
 		fmt.Printf("== Project %s\n", project.Name)
 		for _, result := range project.CheckResults {
 			if result.Success {
-				fmt.Printf("✅ Check %s PASS (labels: %s)\n", result.Name, result.Labels)
+				fmt.Printf("✅ Check %s PASS\n", result.Name)
+				for key, value := range result.Labels {
+					fmt.Printf("\t%s: %s\n", key, value)
+				}
 			} else {
-				fmt.Printf("🚨 Check %s FAILED (labels: %s)\n", result.Name, result.Labels)
-				fmt.Printf("🚨 %+v\n", result)
+				fmt.Printf("🚨 Check %s FAILED\n", result.Name)
+				for key, value := range result.Labels {
+					fmt.Printf("\t%s: %s\n", key, value)
+				}
+				for _, ruleResult := range result.Results {
+					fmt.Printf("\tMessage: %s\n", strings.Join(ruleResult.Messages, ","))
+				}
 			}
 		}
 	}
