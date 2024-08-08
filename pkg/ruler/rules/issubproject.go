8d590221dc9b6c36272ff4ccd7a35d14487bd554
Stefano Celentano <stefano.celentano@qonto.com>
2024-08-08 16:16:20 +0200

    support directories in codeowners

--- pkg/ruler/rules/issubproject.go
+++ pkg/ruler/rules/issubproject.go
@@ -3,6 +3,8 @@ package rules
 import (
 	"context"
 	"errors"
+	"os"
+	"path/filepath"
 
 	"github.com/qonto/standards-insights/pkg/project"
 )
@@ -17,6 +19,16 @@ func NewIsSubProjectRule() *IsSubProjectRule {
 func (rule *IsSubProjectRule) Do(ctx context.Context, project project.Project) error {
 	if project.SubProject == "" {
 		return errors.New("this project is not a subproject")
+	}
+
+	// Check if the SubProject path is a file
+	fileInfo, err := os.Stat(filepath.Join(project.Path, project.SubProject))
+	if err != nil {
+		return err
+	}
+
+	if fileInfo.IsDir() {
+		return errors.New("the SubProject path points to a directory, not a file")
 	}
 
 	return nil
