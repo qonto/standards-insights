208cfa2ff4edd0aa8d1970d0f0426a2397c58a43
Stefano Celentano <stefano.celentano@qonto.com>
2024-08-13 13:08:35 +0200

    fix tests
    Signed-off-by: Stefano Celentano <stefano.celentano@qonto.com>
    Signed-off-by: Stefano Celentano <stefano.celentano87@gmail.com>

--- pkg/codeowners/codeowners_test.go
+++ pkg/codeowners/codeowners_test.go
@@ -11,17 +11,20 @@ func TestCodeowners_FromFile(t *testing.T) {
 		t.Fatalf("CODEOWNERS file does not exist")
 	}
 
-	co := NewCodeowners()
+	co, err := NewCodeowners(".")
+	if err != nil {
+		t.Fatalf("Error creating codeowners: %v", err)
+	}
 
 	tests := []struct {
 		path     string
 		expected string
 	}{
-		{"app/announcers/attachment_announcer.rb", "@team-cft-bookkeeping"},
-		{"app/controllers/v1/external_transfers_controller.rb", "@team-cft-sepa"},
-		{"app/controllers/somepath/v1/external_transfers_controller.rb", "@team-cft-sepa"},
-		{"app/controllers/anotherpath/mandates_controller.rb", "@team-cft-sepa"},
-		{"app/services/transfers/somefile.rb", "@team-cft-sepa"},
+		{"app/announcers/attachment_announcer.rb", "bookkeeping"},
+		{"app/controllers/v1/external_transfers_controller.rb", "sepa"},
+		{"app/controllers/somepath/v1/external_transfers_controller.rb", "sepa"},
+		{"app/controllers/anotherpath/mandates_controller.rb", "sepa"},
+		{"app/services/transfers/somefile.rb", "sepa"},
 	}
 
 	for _, tt := range tests {
