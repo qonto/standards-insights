5fdd865da0abfa6316439d0e41fe83cbe1437c7d
Stefano Celentano <stefano.celentano@qonto.com>
2024-08-14 12:09:39 +0200

    remove hardcoded mapping
    Signed-off-by: Stefano Celentano <stefano.celentano@qonto.com>
    Signed-off-by: Stefano Celentano <stefano.celentano87@gmail.com>

--- pkg/codeowners/codeowners.go
+++ pkg/codeowners/codeowners.go
@@ -34,31 +34,8 @@ func (c *Codeowners) GetOwners(path string) (string, bool) {
 		return "", false
 	}
 
+	// TODO: load ownermap from standard-insights local depoyment file
 	ownerMap := map[string]string{
-		"@team-cft-account-management":          "account-management",
-		"@team-cft-bookkeeping":                 "bookkeeping",
-		"@team-cft-cards":                       "cards",
-		"@team-cft-cash-flow-management":        "cash-flow-management",
-		"@team-cft-connect":                     "connect",
-		"@team-cft-data":                        "data",
-		"@team-cft-data-products":               "data-products",
-		"@team-cft-design-system":               "design-system",
-		"@team-cft-financing":                   "financing",
-		"@team-cft-invoices":                    "invoices",
-		"@team-cft-ledger":                      "ledger",
-		"@team-cft-local-payments":              "local-payments",
-		"@team-cft-onboarding":                  "onboarding",
-		"@team-cft-onboarding-company-creation": "onboarding-company-creation",
-		"@team-cft-onboarding-kyb":              "onboarding-kyb",
-		"@team-cft-onboarding-kyc":              "onboarding-kyc",
-		"@team-cft-onboarding-registration":     "onboarding-registration",
-		"@team-cft-ops-tooling":                 "ops-tooling",
-		"@team-cft-pricing":                     "pricing",
-		"@team-cft-security":                    "security",
-		"@team-cft-sepa":                        "sepa",
-		"@team-cft-spend-management":            "spend-management",
-		"@team-cft-sre":                         "sre",
-		"@team-cft-swift":                       "swift",
 	}
 
 	owner := owners[0]
